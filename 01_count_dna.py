8e79c37db346f57102b47adde9091928c63643b8
Jenn Martinez <61393586+jennimartinez@users.noreply.github.com>
2022-01-02 21:34:48 -0500

    Add files via upload

---
+++ 01_count_dna.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# 01_count_dna.py
+
+# Open file
+with open('data/rosalind_dna.txt', 'r') as input:
+	dna = input.read()
+
+# Create empty list
+nucleo_count = []
+
+# Append count of each nucleotide into nuc_count
+for nucleotide in ['A', 'C', 'G', 'T']:
+	nucleo_count.append(str(dna.count(nucleotide)))
+
+# Check answers
+print ' '.join(nucleo_count)
+
+with open('output/q1_dna_count.txt', 'w') as output:
+	output.write(' '.join(nucleo_count))
---
+++ 02_rna_replace.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# 02_rna_replace.py
+
+# Open input file of dna
+with open('data/rosalind_rna.txt', 'r') as input:
+	# Whitespace located at the end of the dna seq
+	dna = input.read().strip()
+
+# Write output file with transcribed rna
+with open('output/q2_rna.txt', 'w') as output:
+	output.write(dna.replace('T', 'U'))
+
+
+
+
